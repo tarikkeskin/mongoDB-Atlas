81cbef5218d8e74b42156e34993e194812000da5
tarikkeskin <tarik.keskin007@gmail.com>
2021-05-23 17:09:41 +0300

    connect MongoDB-Realm to project

--- realm-web-react-quickstart/src/App.js
+++ realm-web-react-quickstart/src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import * as Realm from "realm-web";
+import * as React from "react";
 
-function App() {
+const REALM_APP_ID = "ceng-495-games-twlxy"; // e.g. myapp-abcde
+const app = new Realm.App({ id: REALM_APP_ID }); // app object represent Realm App
+
+// Create a component that displays the given user's details
+function UserDetail({ user }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Logged in with anonymous id: {user.id}</h1>
     </div>
   );
+}
+
+// Create a component that lets an anonymous user log in
+function Login({ setUser }) {
+  const loginAnonymous = async () => {
+    const user = await app.logIn(Realm.Credentials.anonymous());
+    setUser(user);
+  };
+  return <button onClick={loginAnonymous}>Log In</button>;
+}
+
+const App = () => {
+  // Keep the logged in Realm user in local state. This lets the app re-render
+  // whenever the current user changes (e.g. logs in or logs out).
+  const [user, setUser] = React.useState(app.currentUser);
+  // If a user is logged in, show their details.
+  // Otherwise, show the login screen.
+  return (
+    <div className="App">
+      <div className="App-header">
+        {user ? <UserDetail user={user} /> : <Login setUser={setUser} />}
+      </div>
+    </div>
+  );
+  
 }
 
 export default App;
