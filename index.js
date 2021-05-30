06877167e0c3aed1e4e912740f16d2fca495fbb9
tarikkeskin <tarik.keskin007@gmail.com>
2021-05-30 14:15:47 +0300

    deletions

--- index.js
+++
@@ -1,6 +0,0 @@
-
-const {app} = require('./express/expressApp')
-const port = 3000
-
-var url = "mongodb+srv://keskintarik007:<password>@cluster0.pswt2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-app.set('port')
